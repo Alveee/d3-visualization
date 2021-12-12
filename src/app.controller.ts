65f132988079215582fb27c79337f27cba9bb078
Zahid Hasan Alvi <zahid.hasan@bs-23.net>
2021-12-12 15:37:34 +0600

    Read csv file and get data from csv

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Observable } from 'rxjs';
 import { AppService } from './app.service';
 
+export interface IEntity {
+  state: string;
+  date: string;
+  renewables: number;
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +14,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/data')
+  public async readCsvAndReturnData(): Promise<Observable<IEntity[]>> {
+    return await this.appService.readCsvAndReturnData();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as csv from 'csv-parser';
+import * as getStream from 'get-stream';
+import { Observable, of } from 'rxjs';
+
+export type IEntity = {
+  state: string;
+  date: string;
+  renewables: number;
+};
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  public async readCsvAndReturnData(): Promise<Observable<IEntity[]>> {
+    const filePath = __dirname + '/../storage/interactive_data.csv';
+    const parseStream = csv({});
+    const data = await getStream.array(
+      fs.createReadStream(filePath).pipe(parseStream),
+    );
+    const entities = data.map((x: IEntity) => {
+      return {
+        state: x.state,
+        date: x.date,
+        renewables: x.renewables,
+      };
+    });
+    return of(entities);
   }
 }
