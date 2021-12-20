70653c16a2e168fe3d5ebbf132126c46eb010475
Zahid Hasan Alvi <zahid.hasan@bs-23.net>
2021-12-20 21:14:09 +0600

    Update existing migration and add new script to revert migration

--- src/migrations/1640010840675-CreateFossilFuelTable.ts
+++ src/migrations/1640010840675-CreateFossilFuelTable.ts
@@ -10,6 +10,8 @@ export class CreateFossilFuelTable1640009479662 implements MigrationInterface {
             name: 'id',
             type: 'int',
             isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
           },
           {
             name: 'state',
